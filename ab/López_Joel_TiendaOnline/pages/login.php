fa40848a4c3ba3651686bb8d519f2be8e4da0150
jota2xL <joelleis2005@gmail.com>
2024-01-19 15:38:15 +0100

    primer commit
    proyecto base

---
+++ ab/López_Joel_TiendaOnline/pages/login.php
@@ -0,0 +1,86 @@
+<?php
+// Variables
+$hostDB = '127.0.0.1';
+$nombreDB = 'ab';
+$usuarioDB = 'root';
+$contrasenyaDB = 'Luamomo12#';
+// Conecta con base de datos
+$hostPDO = "mysql:host=$hostDB;dbname=$nombreDB;";
+$miPDO = new PDO($hostPDO, $usuarioDB, $contrasenyaDB);
+// Prepara SELECT
+// $miConsulta = $miPDO->prepare('SELECT * FROM programa;');
+// // Ejecuta consulta
+// $miConsulta->execute();
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de Inicio de Sesión</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        form {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 300px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+        }
+
+        input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 16px;
+            box-sizing: border-box;
+        }
+
+        button {
+            background-color: #4caf50;
+            color: #fff;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+
+<body>
+    <?php if (isset($_POST['boton1'])) {
+        echo "este boton funciona";
+    }
+     ?>
+    <form method="post">
+        <label for="username">Usuario:</label>
+        <input type="text" id="username" name="username" required>
+
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password" required>
+
+        <button name="boton1" type="submit">Iniciar Sesión</button>
+    </form>
+</body>
+
+</html>
